b97b59bbea136319ceb511752ab632ee68374263
Guilherme Schvarcz Franco <guilhermefrancosi@gmail.com>
2013-07-30 12:55:25 -0300

    Add a simulator to test our code.

---
+++ simulator/enviroment.py
@@ -0,0 +1,36 @@
+from morse.builder import *
+
+
+robots = []
+poses = []
+motions = []
+sicks = []
+for i in range(5):
+    robots.append(Pioneer3DX("Robot"+str(i)))
+    robots[i].translate(x=-2.0*i,y=0.0,z=0.0)
+    robots[i].unparent_wheels() 
+
+    motions.append(MotionVWDiff("robot"+str(i)+".motion"))
+    robots[i].append(motions[i])
+
+    poses.append(Pose("robot"+str(i)+".pose"))
+    poses[i].translate(x=0.0,y=0.0,z=0.0)
+    poses[i].rotate(x=0.0,y=0.0,z=0.0)
+    robots[i].append(poses[i])
+
+    sicks.append(Sick("robot"+str(i)+".sick"))
+    sicks[i].translate(x=0.1,y=0.0,z=0.25)
+    sicks[i].properties(Visible_arc=True, laser_range=5.0)
+    robots[i].append(sicks[i])
+
+    protocol = "socket"
+
+    robots[i].add_interface(protocol)
+    poses[i].add_interface(protocol)
+    motions[i].add_interface(protocol)
+    sicks[i].add_interface(protocol)
+
+env = Environment('palco.blend')
+env.place_camera([5, -5, 6])
+env.aim_camera([1.0470, 0, 0.7854])
+
